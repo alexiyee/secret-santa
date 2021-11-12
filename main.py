d62853dd03233c350277b5221fe0330314a8bc05
Voigt, Alexander Leonardo <alexander.leonardo@voigt-cloud.net>
2021-11-12 01:09:01 +0100

    E-mail sending implemented

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from random import randrange
 import sys
+import smtplib
+import ssl
+import getpass
 
 
 def main(argv):
@@ -44,7 +47,25 @@ def main(argv):
         pair[i][1] = shufflelist[i]
         i += 1
 
-#   E-mail transfer
+#   E-mail transfer setup
+    port = 465
+    email = input("Enter gmail adress here: ")
+    password = getpass.getpass(prompt="Enter password for the Mailaccount: ")
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(email, password)
+        sender_email = email
+
+        # send the mails
+        i = 0
+        while i < len(pair):
+            receiver_email = pair[i][0]
+            message = pair[i][1]
+            server.sendmail(sender_email, receiver_email, message)
+            i += 1
+    print("Your looking good!")
 
 
 if __name__ == "__main__":
